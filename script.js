8e8dfbdec9ec3dfc24f29effbb6ddedf0b45dcb6
beqa20088 <138366831+beqa20088@users.noreply.github.com>
2025-06-13 20:55:10 +0400

    kjdasbdk

---
+++ script.js
@@ -0,0 +1,8 @@
+let word = prompt();
+let vowel = [a, e, i, o, u, A, E, I, O, U];
+if (word.include(vowel));{
+    alert('სიტყვა იწყება ხმოვანით, რომელიც არის '(word.charAt(0)))
+}
+else {
+    alert('სიტყვა არ იწყება ხმოვანით, რადგან ის არის'(word.charAt(0)));
+}
